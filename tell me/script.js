72c9518618d8f928d5175c5a7a26054b46791e2c
Jean <jeancarlossenpai@gmail.com>
2022-04-06 19:50:42 -0400

    PH

--- tell me/script.js
+++ tell me/script.js
@@ -1,30 +1,44 @@
 const btn = document.getElementById('btn')
 const post = document.getElementById('source')
 const download = document.getElementById('download')
+let p = document.getElementById('text')
+var textToAdd = document.createTextNode(`w: ${window.screen.availWidth} h: ${window.screen.availHeight}`);
+p.appendChild(textToAdd)
 
-btn.addEventListener('click', getJoke)
+btn.addEventListener('click', changeBackground)
 
-
-//Calling the API
-// async function getJoke(){
-//     const request =  await fetch('https://animechan.vercel.app/api/random')
-//     const {quote, anime} = await request.json()
-
-//     let utterance = new SpeechSynthesisUtterance(`${quote} From the anime = ${anime}`);
-//     speechSynthesis.speak(utterance);
-    
-//     console.log()
-// }
-
-async function getJoke(){
-    const request =  await fetch('https://api.waifu.im/random/?is_nsfw=false')
+async function changeBackground(){
+    const request =  await fetch('https://api.waifu.im/random/?is_nsfw=false&many=true&full=false')
     const response = await request.json()
-    //const {} = response[0]
-    const {dominant_color, url, source} = response.images[0]
-    console.table(response.images[0])
+    let bestPhoto = perfectImage(response.images)
 
-    document.body.style.background = `${dominant_color} url(${url}) no-repeat center center fixed`;
+    //body
+    document.body.style.background = `${bestPhoto.dominant_color} url(${bestPhoto.url}) no-repeat center center fixed`;
     document.body.style.backgroundSize = 'cover';
-    Object.assign(post, {href: source})
-    Object.assign(download, {href: url})
+
+    addAttribute([post,download], {href: bestPhoto.source})
+    buttonColor(post, bestPhoto.dominant_color)
+    buttonColor(download, bestPhoto.dominant_color)   
+
+}
+
+function buttonColor(element, color){
+    element.style.background = color
+}
+
+function addAttribute(arr, obj){
+    arr.forEach(element => {
+        Object.assign(element, obj)
+    });
+}
+
+function perfectImage(photos){
+   for(let i = 0; i < photos.length;i++){
+       console.log(photos[i].width)
+    if(parseInt(photos[i].width) <= window.screen.availHeight ){
+        console.log(`Actual image: ${parseInt(photos[i].width)} screenW: ${window.screen.availWidth}`)
+        return {dominant_color, url, source} = photos[i] 
+    }
+   }
+   
 }
