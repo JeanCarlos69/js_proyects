bf1e3e5c17632aa4bdf915a40a0976fcfab423dd
Jean Carlos Farias Nina <jfarias@indrhi.local>
2022-04-07 15:33:02 -0400

    Page almost done

--- tell me/script.js
+++ tell me/script.js
@@ -1,44 +1,97 @@
+
 const btn = document.getElementById('btn')
 const post = document.getElementById('source')
 const download = document.getElementById('download')
-let p = document.getElementById('text')
-var textToAdd = document.createTextNode(`w: ${window.screen.availWidth} h: ${window.screen.availHeight}`);
-p.appendChild(textToAdd)
+const versatile = document.getElementById('versatile')
+const category = document.getElementById('ctg')
+
+const checkbox = document.getElementById('n')
+const cache = {}
 
 btn.addEventListener('click', changeBackground)
-
+//This is the main function
 async function changeBackground(){
-    const request =  await fetch('https://api.waifu.im/random/?is_nsfw=false&many=true&full=false')
+    const request =  await fetch(linkToUse())
     const response = await request.json()
-    let bestPhoto = perfectImage(response.images)
-
-    //body
-    document.body.style.background = `${bestPhoto.dominant_color} url(${bestPhoto.url}) no-repeat center center fixed`;
+    const bestPhoto = imageProperties(response.images)
+    //Body style
+    document.body.style.background = `${bestPhoto.dominant_color} url(${bestPhoto?.url}) no-repeat center center fixed`;
     document.body.style.backgroundSize = 'cover';
 
-    addAttribute([post,download], {href: bestPhoto.source})
-    buttonColor(post, bestPhoto.dominant_color)
-    buttonColor(download, bestPhoto.dominant_color)   
-
+    post.href = bestPhoto?.source
+    setAttributes(download, {download: bestPhoto?.file, href: bestPhoto?.url})
+    buttonColor([post,download,btn,versatile,category], bestPhoto.dominant_color) 
 }
 
-function buttonColor(element, color){
-    element.style.background = color
+
+function buttonColor(elements, color){
+    elements.forEach(element =>{
+        element.style.background = color
+    })
 }
 
-function addAttribute(arr, obj){
-    arr.forEach(element => {
-        Object.assign(element, obj)
-    });
-}
+//Set Atributes
+function setAttributes(el, attrs) {
+    for(let key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
 
-function perfectImage(photos){
-   for(let i = 0; i < photos.length;i++){
-       console.log(photos[i].width)
-    if(parseInt(photos[i].width) <= window.screen.availHeight ){
-        console.log(`Actual image: ${parseInt(photos[i].width)} screenW: ${window.screen.availWidth}`)
-        return {dominant_color, url, source} = photos[i] 
+  //parseInt(photos[i].width
+
+//Chose an image == || > than the width of the curren device
+function imageProperties(photos){
+   //TODO: default values
+   let defaultValue = {
+    dominant_color: '#f5f5f5'
+   }
+    for(let i = 0; i < photos.length;i++){ 
+        _id = photos[i].image_id //Getting the individual tag  
+        if (cache.hasOwnProperty(_id)){
+            //Destructury to get just what we need
+            console.count()
+            return {dominant_color, url, source,file} = cache[_id]
+        }else {
+        //We create the property with the id of the image and use destructuring 
+        //to get just what we'll ned
+        cache[_id] = {dominant_color, url, source,file} = photos[i]
+        return  cache[_id]
     }
    }
-   
+
+   return defaultValue
 }
+
+function downloader(url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.target = "_blank"
+    a.download = url.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+  }
+
+  function linkToUse(){
+      const BASE_URL =  `https://api.waifu.im/random/?is_nsfw=${checkbox.checked}`
+      if(versatile.value){
+          return BASE_URL + `&selected_tags=${versatile.value}&many=true&full=false`
+      }
+
+      return `https://api.waifu.im/random/?is_nsfw=${checkbox.checked}&many=true&full=false`
+  }
+
+  async function gettingTags(){
+      const request = await fetch("https://api.waifu.im/tags/?full=false")
+      const response = await request.json()
+
+      response.versatile.forEach(element => {
+          const option_tag = document.createElement('option')
+          const text = document.createTextNode(element.charAt(0).toUpperCase() + element.slice(1)) //first letter uppercase
+          setAttributes(option_tag, {value: element})
+          option_tag.appendChild(text) 
+          versatile.appendChild(option_tag)
+      });
+  }
+
+gettingTags()
